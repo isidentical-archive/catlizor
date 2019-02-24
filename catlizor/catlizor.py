5faa4daecc401e487781f077c7e7b79146b9f71a
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-02-24 12:56:28 +0300

    Add call handling

--- catlizor/catlizor.py
+++ catlizor/catlizor.py
@@ -2,6 +2,7 @@ from collections import defaultdict, ChainMap
 from functools import wraps, partial
 from contextlib import contextmanager
 
+SIGNATURE = "__catlized"
 WATCH_ATTRIB_METHS = {"__getattribute__", "__setattr__", "__delattr__"}
 WATCH_ATTRIB_ALIAS = {
     "get": "__getattribute__",
@@ -13,6 +14,9 @@ WATCH_ATTRIB_ALIAS = {
 def attrib_wrapper(hooker, f):
     @wraps(f)
     def wrapper(*args, **kwargs):
+        if "name" in kwargs:
+            kwargs["_name"] = kwargs.pop("name")
+
         with hooker.catch(f.__name__, *args, **kwargs) as c:
             val = c(f(*args, **kwargs))
         return val
@@ -21,10 +25,12 @@ def attrib_wrapper(hooker, f):
 
 
 class AttribWatchHooks:
-    def __init__(self, *watches):
-        self.pre_hooks = defaultdict(list)
-        self.post_hooks = defaultdict(list)
-        self.value_hooks = defaultdict(list)
+    HOOKS = ("pre", "post", "value")
+
+    def __init__(self, watches):
+        for hook in self.HOOKS:
+            setattr(self, f"{hook}_hooks", defaultdict(list))
+
         self.exclude = defaultdict(list)
 
         self._last_val = None
@@ -40,7 +46,8 @@ class AttribWatchHooks:
                             map(WATCH_ATTRIB_ALIAS.get, aliases)
                         )
 
-    def exc(self, cond, name, *args, **kwargs):
+    def exc(self, cond, name, args, kwargs):
+
         excluded = self.exclude.get(f"{cond}_{args[1]}")
         if (not excluded) or (name not in excluded):
             hooks = getattr(self, f"{cond}_hooks")[args[1]]
@@ -50,30 +57,63 @@ class AttribWatchHooks:
     @contextmanager
     def catch(self, name, *args, **kwargs):
         try:
-            self.exc("pre", name, *args, **kwargs)
+            self.exc("pre", name, args, kwargs)
             yield self
-            self.exc(
-                "value", name, *args, **ChainMap(kwargs, {"value": self._last_val})
-            )
+            self.exc("value", name, args, ChainMap(kwargs, {"value": self._last_val}))
             self._last_val = None
         finally:
-            self.exc("post", name, *args, **kwargs)
+            self.exc("post", name, args, kwargs)
 
     def __call__(self, value):
         self._last_val = value
         return value
 
 
-def catlizor(cls=None, *, watch=None):
+class CallWatchHooks(AttribWatchHooks):
+    def __init__(self, watches):
+        for hook in self.HOOKS:
+            setattr(self, f"{hook}_hooks", defaultdict(list))
+
+        self.methods = []
+        for watch in watches:
+            self.methods.extend(watch["funcs"])
+            for meth in watch["funcs"]:
+                if "hooks" in watch:
+                    for hook, fns in watch["hooks"].items():
+                        getattr(self, f"{hook}_hooks")[meth].extend(fns)
+
+    def __iter__(self):
+        return iter(self.methods)
+
+    def exc(self, cond, name, args, kwargs):
+        hooks = getattr(self, f"{cond}_hooks")[name]
+        for hook in hooks:
+            hook(name, *args, **kwargs)
+
+
+def catlizor(cls=None, *, watch=None, sign=True):
     def wrapper(cls):
         if watch:
-            watch_hooks = AttribWatchHooks(*watch)
-            hooked_wrapper = partial(attrib_wrapper, watch_hooks)
+            attr_watch_hooks = AttribWatchHooks(
+                filter(lambda watch: watch.get("attribs"), watch)
+            )
+            hooked_attr_wrapper = partial(attrib_wrapper, attr_watch_hooks)
 
             for meth in WATCH_ATTRIB_METHS:
-                setattr(cls, meth, hooked_wrapper(getattr(cls, meth)))
+                setattr(cls, meth, hooked_attr_wrapper(getattr(cls, meth)))
 
-        cls.__catlized = True
+            if any(filter(lambda watch: watch.get("funcs"), watch)):
+                call_watch_hooks = CallWatchHooks(
+                    filter(lambda watch: watch.get("funcs"), watch)
+                )
+                hooked_call_wrapper = partial(attrib_wrapper, call_watch_hooks)
+
+                for meth in call_watch_hooks:
+                    setattr(cls, meth, hooked_call_wrapper(getattr(cls, meth)))
+
+        if sign:
+            setattr(cls, SIGNATURE, True)
+
         return cls
 
     if cls is None:
--- example.py
+++ example.py
@@ -1,52 +1,45 @@
 from catlizor import catlizor
 from functools import partial
 
-
 def fancy_print(st, name, *args, **kwargs):
     print(f"{st:8} {name} call with {args} and {kwargs}")
-
-
+    
 options = {
     "watch": [
         {
             "attribs": ["name"],
             "hooks": {
-                "pre": [partial(fancy_print, "pre")],
-                "post": [partial(fancy_print, "post")],
-                "value": [partial(fancy_print, "value")],
+                "pre": [partial(fancy_print, 'pre')],
+                "post": [partial(fancy_print, 'post')],
+                "value": [partial(fancy_print, 'value')]
             },
             "exclude": {
                 "pre": ["set", "del"],
                 "post": ["get"],
-                "value": ["set", "del"],
-            },
+                "value": ["set", "del"]
+            }
         },
         {
-            "attribs": ["age"],
+            "funcs": ["task"],
             "hooks": {
-                "value": [
-                    lambda name, *a, **k: print(
-                        f"Got value of {name}({', '.join(a[1:])}), {k['value']}"
-                    )
-                ]
+                "value": [lambda name, *a, **k: print(f"Got value of {name}({', '.join(a[1:])}), {k['value']}")]
             },
-            "exclude": {"value": ["set", "del"]},
-        },
+        }
     ]
 }
-
 
 @catlizor(**options)
 class MyClass:
     def __init__(self, name, age=15):
         self.name = "batuhan"
         self.age = age
-
     def __repr__(self):
         return "MyClass instance"
-
-
+    def task(self, *args, **kwargs):
+        return (args, kwargs)
+        
 mc = MyClass("batuhan")
 mc.__dict__["birth"] = 2019 - mc.age
 info = f"{mc.name} {mc.age} years old."
 del mc.name
+mc.task(pretty=True)
