e833c385379f637f7652b2776c7ac9bb5e67f130
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-02-24 11:02:51 +0300

    packaging

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
+from pathlib import Path
 from setuptools import setup
+
+current_dir = Path(__file__).parent.resolve()
+
+with open(current_dir / 'README.md', encoding='utf-8') as f:
+    long_description = f.read()
+    
 setup(
     name = 'catlizor',
     version = '0.1',
     author = 'BTaskaya',
     author_email = 'batuhanosmantaskaya@gmail.com',
-    packages = ['catlizor']
+    packages = ['catlizor'],
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/btaskaya/catlizor'
 )
