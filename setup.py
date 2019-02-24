86e53c31907e94a5164ebbc72c7793fdc3a7bd1a
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-02-24 12:58:17 +0300

    bump version and require python version greater equal than 3.7

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ with open(current_dir / 'README.md', encoding='utf-8') as f:
     
 setup(
     name = 'catlizor',
-    version = '0.1.1',
+    version = '0.2',
     author = 'BTaskaya',
     author_email = 'batuhanosmantaskaya@gmail.com',
     packages = ['catlizor'],
     description = "Watch python classes",
     long_description = long_description,
     long_description_content_type = 'text/markdown',
-    url = 'https://github.com/btaskaya/catlizor'
+    url = 'https://github.com/btaskaya/catlizor',
+    python_requires='>=3.7'
 )
