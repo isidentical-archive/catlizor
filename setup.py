668b572d048dc809f2ea2929d15434023d462fdc
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-02-24 11:05:42 +0300

    add desc

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ with open(current_dir / 'README.md', encoding='utf-8') as f:
     
 setup(
     name = 'catlizor',
-    version = '0.1',
+    version = '0.1.1',
     author = 'BTaskaya',
     author_email = 'batuhanosmantaskaya@gmail.com',
     packages = ['catlizor'],
+    description = "Watch python classes",
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/btaskaya/catlizor'
