f60314eb4c43378ba77e321b6fbef21e3a944f97
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-03-02 17:38:14 +0300

    packagizing

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 from pathlib import Path
-from setuptools import setup
+from setuptools import setup, Extension
 
 current_dir = Path(__file__).parent.resolve()
 
 with open(current_dir / 'README.md', encoding='utf-8') as f:
     long_description = f.read()
-    
+
+hookify = Extension(
+    name = 'hookify',
+    sources = ['hookify/hookify.c'],
+    include_dirs = ['hookify']
+)
+ 
 setup(
     name = 'catlizor',
-    version = '1.0.0',
+    version = '1.9.0', # 1.9.x series for v1-extended
     author = 'BTaskaya',
     author_email = 'batuhanosmantaskaya@gmail.com',
     packages = ['catlizor'],
+    ext_modules = [hookify],
     description = "Action Hooks.",
     long_description = long_description,
     long_description_content_type = 'text/markdown',
